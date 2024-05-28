4b3b9707ae5df0eec6ce2c090dd50096fe717ecb
orgulhoduelista <00001089116974SP@al.educacao.sp.gov.br>
2024-05-28 08:31:30 -0300

    artisco 1.0

---
+++ sketch.js
@@ -0,0 +1,19 @@
+unction setup() {
+    createCanvas(4000,4000);
+    background(500);
+  }
+  
+    function draw() {
+      
+    stroke("black")
+    fill("purple")  
+    
+    //console.log.(mouseIspnecsed);
+   
+      
+    if(mouseIsPressed){
+    rect(mouseX,mouseY,10,25);
+     }  
+  
+    
+  }
